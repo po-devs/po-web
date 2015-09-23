f967b5fc7fa09ad5f7e9a73077cfc20a000291d7
coyotte508 <coyotte508@gmail.com>
2015-09-23 23:21:02 +0200

    Add some chat colors

--- app/assets/javascript/frontend.js
+++ app/assets/javascript/frontend.js
@@ -9,7 +9,40 @@ var webclientUI = {
             return webclientUI.printHtml(msg);
         }
 
-        webclientUI.insertMessage(utils.stripHtml(msg));
+        // if (html) {
+        //     msg = webclient.convertImages($("<div>").html(msg)).html();
+        // } else {
+        msg = utils.escapeHtml(msg);
+
+        if (msg.substr(0, 3) === "***") {
+            msg = "<span class='action'>" + msg + "</span>";
+        } else if (msg.indexOf(":") !== -1) {
+            var pref = msg.substr(0, msg.indexOf(":"));
+            var id = -1;
+            var auth = 0;
+            // id = webclient.players.id(pref);
+            // auth = webclient.players.auth(id);
+
+            // if (webclient.players.isIgnored(id)) {
+            //     return;
+            // }
+
+            if (pref === "~~Server~~") {
+                pref = "<span class='server-message'>" + pref + ":</span>";
+            } else if (pref === "Welcome Message") {
+                pref = "<span class='welcome-message'>" + pref + ":</span>";
+            } else if (id === -1) {
+                pref = "<span class='script-message'>" + pref + ":</span>";
+            } else {
+                pref = "<span class='player-message' style='color: " + webclient.players.color(id) + "'>" + utils.rank(auth) + utils.rankStyle(pref + ":", auth) + "</span>";
+                this.activateTab();
+            }
+
+            //msg = pref + utils.addChannelLinks(msg.slice(msg.indexOf(":") + 1), webclient.channels.channelsByName(true));
+            msg = pref + msg.slice(msg.indexOf(":") + 1);
+        }
+
+        webclientUI.insertMessage(msg);
         //$(".chat").html($(".chat").html() + utils.stripHtml(msg) + "<br>");
     },
 
