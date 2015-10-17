806cbdf235fee5ae0047af57e26a400e19b50ab1
SteveC688 <stevec8@live.co.uk>
2015-10-17 15:13:46 +0100

    Add ignore pokeballs. Closes #26
    Add ignore pokeballs. Closes #26

--- app/assets/javascript/frontend.js
+++ app/assets/javascript/frontend.js
@@ -355,6 +355,7 @@ $(function() {
                     } else {
                         webclient.players.removeIgnore(pid);
                     }
+                    webclientUI.players.updatePlayer(+pid);
                 }
             } else if (cmd === "watch") {
                 network.command('watch', {battle: +payload});
