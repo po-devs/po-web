ca7bcf6f5ee2af0171f451a497382d6f370e426d
coyotte508 <coyotte508@gmail.com>
2016-05-15 11:05:22 +0200

    remove favicon animations altogether
    otherwise for exampe if it was at 1, i read the pm,
    it disappears, then someone talks in anotehr chat, i'd
    see the 1, then animate to disappear, then animate to 0.

--- app/assets/javascript/notif.js
+++ app/assets/javascript/notif.js
@@ -2,26 +2,15 @@
 	Notif
 */
 var notif = {
-    favicon: new Favico({
-        animation: "pop",
-        type: "circle"
-    }),
     faviconNoAnim: new Favico({
         animation: "none",
         type: "circle"
     }),
     count: -1,
-    one_count: 0,
     titleUpdate: function() {
         if (notif.count >= 0) {
-            if (notif.one_count) {
-                notif.faviconNoAnim.badge(""+notif.count);
-            } else {
-                notif.one_count = 1;
-                notif.favicon.badge(""+notif.count);
-            }
+            notif.faviconNoAnim.badge(""+notif.count);
         } else {
-            notif.one_count = 0;
             notif.faviconNoAnim.badge("");
         }
     }
