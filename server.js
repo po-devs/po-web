e5e3f87c2999843af7deca33e95633976d8d2645
coyotte508 <coyotte508@gmail.com>
2016-05-23 21:08:09 +0200

    Send compressed data when possible

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var path = require('path');
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
+var compression = require('compression');
 
 var config = require('./serverconfig.js');
 
 var app = express();
+
+app.use(compression());
 
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
