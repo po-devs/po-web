a66909c22b32b208b363a75a71a222dc7626c4ba
coyotte508 <coyotte508@gmail.com>
2017-02-02 01:52:11 +0100

    Group common files in frontend.js and teambuilder.js
    Related to #184

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,7 @@ module.exports = {
     ]
   },
   plugins: [
+    new webpack.optimize.CommonsChunkPlugin("common"),
     new webpack.ProvidePlugin({ $: 'jquery', jquery: 'jquery', jQuery: 'jquery' })
   ]
 }
